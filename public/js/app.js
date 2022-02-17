de5fd2d00dbc9157253dc8877c67017a4ac100ca
francesca <fradeluca85@gmail.com>
2022-02-17 15:40:31 +0100

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /media/francesca/Elements/Boolean/classe_47/laravel/laravel-auth/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /media/francesca/Elements/Boolean/classe_47/laravel/laravel-auth/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /media/francesca/Elements/Boolean/classe_47/laravel/laravel-one-to-many/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /media/francesca/Elements/Boolean/classe_47/laravel/laravel-one-to-many/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
