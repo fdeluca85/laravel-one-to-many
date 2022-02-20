9768eb88dad6b95cc57b1d49ecbd955de3e4cbe8
francesca <fradeluca85@gmail.com>
2022-02-20 22:12:46 +0100

    delete

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -174,6 +174,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
         $post->delete();
 
         return redirect()->route("posts.index",);
