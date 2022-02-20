de6a6dc36d1d5949159d099b1b87308ccedf75a3
francesca <fradeluca85@gmail.com>
2022-02-20 22:10:13 +0100

    add actions button show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,14 +5,24 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header"><h2>{{$post->title}}</h2></div>
+                <div class="card-header"><h2>{{$post->title}}</h2>
+                    <p>
+                        <a  href="{{route("posts.edit", $post->id)}}"><button type="button" class="btn btn-warning">Modifica</button></a>
+                    </p>
+                    <p>
+                        <form action="{{route("posts.destroy", $post->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-danger">Elimina</button>
+                        </form>
+                    </p>
+                </div>
                 <div class="card-body">
                     <div class="mb-2"><strong>Stato:</strong> 
                         @if ($post->published)
                         <span class="badge badge-success">Pubblicato</span>
                         @else
-                        <span class="badge badge-secondary">Bozza</span>
-                                                        
+                        <span class="badge badge-secondary">Bozza</span>                                                        
                         @endif
                     </div>
                     <div class="mb-3">
